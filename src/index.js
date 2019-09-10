e4664bea6fa0cca61565bcc49471259bfbcadec5
Vladimir Sulimov <Sulimov>
2019-09-10 20:28:11 +0300

    homework 6, not finished

--- src/index.js
+++ src/index.js
@@ -1,92 +1,66 @@
-/* ДЗ 5 - DOM Events */
+/* ДЗ 6 - Асинхронность и работа с сетью */
 
 /*
  Задание 1:
 
- Функция должна добавлять обработчик fn события eventName к элементу target
+ Функция должна возвращать Promise, который должен быть разрешен через указанное количество секунду
 
  Пример:
-   addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
+   delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
-function addListener(eventName, target, fn) {
-    target.addEventListener(eventName, fn);
-
+function delayPromise(seconds) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, seconds);
+    })
 }
 
 /*
  Задание 2:
 
- Функция должна удалять у элемента target обработчик fn события eventName
+ 2.1: Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов пожно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+
+ 2.2: Элементы полученного массива должны быть отсортированы по имени города
 
  Пример:
-   removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
+   loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
-function removeListener(eventName, target, fn) {
-    target.removeEventListener(eventName, fn);
-}
+function loadAndSortTowns() {
 
-/*
- Задание 3:
+    return new Promise((resolve) => {
 
-     Функция должна добавить к элементу target такой обработчик на события eventName, чтобы он отменял действия по умолчанию
+        let xhr = new XMLHttpRequest();
 
-     Пример:
-   skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
- */
-function skipDefault(eventName, target) {
-    target.addEventListener(eventName, () => event.preventDefault());
-}
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json', true);
 
-/*
- Задание 4:
+        xhr.send();
 
- Функция должна эмулировать событие click для элемента target
+        xhr.onload = function () {
+            let towns = JSON.parse(xhr.response);
 
- Пример:
-   emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
- */
-function emulateClick(target) {
-    let event = new Event('click');
+            if (Array.isArray(towns)) {
+                towns.sort((a, b) => {
+                    if (a.name < b.name) {
 
-    target.dispatchEvent(event);
-}
+                        return -1;
 
-/*
- Задание 5:
+                    } else if (a.name > b.name) {
 
- Функция должна добавить такой обработчик кликов к элементу target,
- который реагирует (вызывает fn) только на клики по элементам BUTTON внутри target
+                        return 1;
+                    }
 
- Пример:
-   delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
- */
-function delegate(target, fn) {
-    target.onclick = function(event) {
-        if (event.target.tagName === 'BUTTON') {
-            fn();
+                    return 0;
+
+                });
+                resolve(towns);
+            }
         }
-    };
-}
-
-/*
- Задание 6:
-
- Функция должна добавить такой обработчик кликов к элементу target,
- который сработает только один раз и удалится (перестанет срабатывать для последующих кликов по указанному элементу)
-
- Пример:
-   once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
- */
-
-function once(target, fn) {
-    target.addEventListener('click', fn, { once: true });
+    })
 }
 
 export {
-    addListener,
-    removeListener,
-    skipDefault,
-    emulateClick,
-    delegate,
-    once
+    delayPromise,
+    loadAndSortTowns
 };
