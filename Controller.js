a08083aa318fbe1977ae9c8053005964d5f41305
Vladimir Sulimov <Sulimov>
2019-09-16 23:16:45 +0300

    Сохранение

--- Controller.js
+++ Controller.js
@@ -1,7 +1,18 @@
 window.Controoler = {
-  async friends(){
-      const my_friends = document.getElementById('my_friends');
-      const friends = await Model.getFriends({fields: 'photo_100'});
-      my_friends.innerHTML = View.render('friends', {list: friends.items});
-  }
+    async friends() {
+        const MyFriends = document.querySelector('#my_friends');
+        // const ListFriends = document.querySelector('#list_friends');
+        const friends = await Model.getFriends({fields: 'photo_100'});
+        // const list = JSON.parse(localStorage.getItem('mylist')) || [];
+        // const bestfriends = [];
+        // friends.items.forEach(friend => {
+        //     console.log(list.indexOf(String(friend.id)));
+        //     if (list.indexOf(String(friend.id)) !== -1){
+        //         bestfriends.push(friend);
+        //     }
+        // });
+        //
+        MyFriends.innerHTML = View.render('friends', {list: friends.items});
+        // ListFriends.innerHTML = View.render('friends', {list: bestfriends});
+    }
 };
